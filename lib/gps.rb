eb2e82b6f54a78a99b1ed9d13b428e40aa8f7b92
Zaid Al Lahham <zlahham@gmail.com>
2016-02-25 23:12:34 +0000

    find_route works

--- lib/gps.rb
+++ lib/gps.rb
@@ -1,5 +1,27 @@
 class GPS
+  ROUTES = {
+    'AB': 5,
+    'BC': 4,
+    'CD': 7,
+    'DC': 8,
+    'DE': 6,
+    'AD': 5,
+    'CE': 2,
+    'EB': 3,
+    'AE': 7
+  }.freeze
+
+  def initialize
+    @current_route = {}
+  end
+
   def find_distance(start, finish)
-    5 if start == 'A' && finish == 'B'
+    if start == 'A' && finish == 'B'
+      5
+    end
+  end
+
+  def lookup_route(start, finish)
+    ROUTES.select { |key, _value| key == "#{start}#{finish}".to_sym }.flatten
   end
 end
--- spec/gps_spec.rb
+++ spec/gps_spec.rb
@@ -1,4 +1,6 @@
 describe GPS do
+  let(:routes) { GPS::ROUTES }
+
   context '#initialize' do
     it 'starts empty' do
       expect(subject).to be_instance_of(GPS)
@@ -8,6 +10,12 @@ describe GPS do
   context '#find_distance' do
     it 'reports back the distance from start to end' do
       expect(subject.find_distance('A', 'B')).to eq 5
+    end
+  end
+
+  context '#lookup_route' do
+    it 'reports back the existance of a valid route' do
+      expect(subject.lookup_route('A', 'B')).to eq routes.first
     end
   end
 end
