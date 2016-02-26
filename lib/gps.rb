f47ba3a927ca5a6e676e78fdba982a30bd92685b
Zaid Al Lahham <zlahham@gmail.com>
2016-02-26 00:32:00 +0000

    added more tests

--- lib/gps.rb
+++ lib/gps.rb
@@ -1,25 +1,30 @@
+# frozen_string_literal: true
+
 class GPS
-  ROUTES = {
-    'AB': 5,
-    'BC': 4,
-    'CD': 7,
-    'DC': 8,
-    'DE': 6,
-    'AD': 5,
-    'CE': 2,
-    'EB': 3,
-    'AE': 7
-  }.freeze
+  ROUTES = { 'AB': 5, 'BC': 4, 'CD': 7,
+             'DC': 8, 'DE': 6, 'AD': 5,
+             'CE': 2, 'EB': 3, 'AE': 7
+           }.freeze
+
+  JUNCTIONS = %w(A B C D E).freeze
 
   def initialize
     @current_route = {}
   end
 
   def find_distance(start, finish)
-    lookup_route(start, finish).last
+    if route_exists?(start, finish) == false
+      false
+    else
+      lookup_route(start, finish).last
+    end
   end
 
   def lookup_route(start, finish)
     ROUTES.select { |key, _value| key == "#{start}#{finish}".to_sym }.flatten
+  end
+
+  def route_exists?(start, finish)
+    lookup_route(start, finish) == [] ? false : true
   end
 end
--- spec/gps_spec.rb
+++ spec/gps_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe GPS do
   let(:routes) { GPS::ROUTES }
 
@@ -11,11 +13,24 @@ describe GPS do
     it 'reports back the distance from start to end' do
       expect(subject.find_distance('A', 'B')).to eq 5
     end
+    it 'does not report distances for invalid entries' do
+      expect(subject.find_distance('X', 'U')).to eq nil
+    end
   end
 
   context '#lookup_route' do
     it 'reports back the existance of a valid route' do
       expect(subject.lookup_route('A', 'B')).to eq routes.first
+    end
+  end
+
+  context '#route_exists' do
+    it 'validates the presence of a route' do
+      expect(subject.route_exists?('A', 'B')).to be true
+    end
+
+    it 'does not validate the non-presence of a route' do
+      expect(subject.route_exists?('A', 'C')).to be false
     end
   end
 end
